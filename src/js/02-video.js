0b6f29a797dbd0e5e3adfb058e8c12836c7234e2
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2023-07-06 20:20:52 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Vimeo from "@vimeo/player";
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector("#vimeo-player");
+
+
+const iframePlayer = new Vimeo(iframe);
+
+iframePlayer.on("timeupdate", throttle((time) => {
+return localStorage.setItem("videoplayer-current-time", JSON.stringify(time.seconds))
+}, 1000));
+
+iframePlayer.setCurrentTime(Number.parseFloat(localStorage.getItem("videoplayer-current-time")))
