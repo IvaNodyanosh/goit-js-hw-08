40acc795abe0e96502cf1041d958eba64fda4a3c
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2023-07-06 17:19:14 +0300

    added task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,23 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+
+const items = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+}).join('');
+
+gallery.innerHTML = items;
+
+new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
