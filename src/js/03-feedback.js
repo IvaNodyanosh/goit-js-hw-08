b69ee47955b9655810215c30860632f9941c2593
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2023-07-07 13:28:58 +0300

    added task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+const throttle = require('lodash.throttle');
+
+
+const form = document.querySelector("form");
+const button = document.querySelector("button");
+console.log(form);
+console.log(button);
+
+
+
+
+
+
+const addData = function () {
+    localStorage.setItem("feedback-form-state", JSON.stringify({ email: form.email.value, message: form.message.value }));
+}
+
+let data
+
+try {
+    data = JSON.parse(localStorage.getItem("feedback-form-state"));
+    form.email.value = data.email;
+    form.message.value = data.message;
+} catch (error) {
+    console.log(error.message);
+}
+
+
+
+
+
+
+const formSubmit = function (e) {
+
+    e.preventDefault();
+
+    console.log(data)
+
+    
+
+    form.email.value = "";
+    form.message.value = "";
+
+    localStorage.removeItem("feedback-form-state");
+
+}
+
+
+form.addEventListener("input", throttle(() => {
+    addData()
+  }, 1000));
+
+
+form.addEventListener("submit", (event) => formSubmit(event))
+
+
+
+
+
 
